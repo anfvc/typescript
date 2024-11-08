f379ff6ebc70ac8063fdefcbf2881b66f6db283b
andresvillay <andresvillay2@gmail.com>
2024-11-08 15:48:30 +0100

    learning more about types in functions

--- 02Basics/myFunctions.ts
+++ 02Basics/myFunctions.ts
@@ -1,5 +1,6 @@
-function addTwo(number: number) {
+function addTwo(number: number): number /* this should return a number */ {
   return number + 2;
+  // return "hello"; //if I pass a string, why is it right? //* We need to specify the type of the function
 }
 
 function getUpper(value: string) {
@@ -24,5 +25,35 @@ addTwo(10);
 getUpper("andres");
 signUpUser("Andres", "andres@mail.com", "hello123!", true);
 loginUser("andres", "andres@mail.com");
+
+//* Returning more than one type:
+// function getValue(myVal: number): boolean {
+//   if (myVal > 5) {
+//     return true;
+//   }
+
+//   return "200 OK";
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heroes = ["thor", "spider-man", "ironman"]; //TS will expect a string
+// const heroes = [1, 2, 3]; //TS will expect a number
+
+//!No need to specify the type
+heroes.map((hero /* : string */): string => {
+  return `Hero is ${hero}`;
+});
+
+function consoleError(errormsg: string): void {
+  console.log(errormsg);
+  return;
+}
+
+function handleError(errormsg: string): never {
+  throw new Error(errormsg)
+}
 
 export {};
